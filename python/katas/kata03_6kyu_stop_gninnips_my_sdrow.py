e3fd41380428069b02cbbd15b04f17122e5c936e
titor oopart <johnpiterzon@gmail.com>
2025-11-09 04:57:34 -0500

    python kata03 6kyu stop gninnips my sdrow.

---
+++ python/katas/kata03_6kyu_stop_gninnips_my_sdrow.py
@@ -0,0 +1,17 @@
+"""
+Write a function that takes in a string of one or more words, and returns the same string, but with all words that have five or more letters reversed (just like the name of this kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+
+Examples:
+
+"Hey fellow warriors"  --> "Hey wollef sroirraw"
+"This is a test        --> "This is a test"
+"This is another test" --> "This is rehtona test"
+"""
+
+
+def spin_words(sentence):
+    sentense_arr = sentence.split(" ")
+    result = []
+    for word in sentense_arr:
+        result.append(word if len(word) <= 4 else word[::-1])
+    return " ".join(result)
